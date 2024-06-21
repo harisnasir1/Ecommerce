12b501ef01c12ee322b94902f1e5fd380df7f0b0
Haris <hnm@opinionai.ai>
2024-06-21 20:52:22 +0500

    vercel config fix try 1

--- frontend/src/Utils/apiroutes.js
+++ frontend/src/Utils/apiroutes.js
@@ -1,5 +1,5 @@
 
-export const host="http://localhost:5000";
+export const host="https://ecommerce-inky-five.vercel.app/";
 export const SignupRoute=`${host}/api/auth/adminSignup`;
 export const LoginRoute =`${host}/api/auth/adminLogin`;
 //products
